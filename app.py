59122aa78b3106facd4565a92d9c096f1bb4b821
Natasha <snatasha11@gmail.com>
2024-11-13 15:10:15 -0600

    Lección Streamlet - Semana 9

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st 
+
+st.title("Proyecto Natasha")
+
+st.header("Objetivo del Informe")
+
+st.write("El objetivo de este informe es analizar las ventas de productos en la base de datos Northwind, enfocándonos en identificar los productos y categorías más vendidos.")
+st.write("Este análisis permitirá formular recomendaciones orientadas a optimizar la estrategia de ventas y la gestión de inventario en función de los datos obtenido")
